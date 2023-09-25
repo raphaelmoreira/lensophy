51d5aa30df6e92b06273ef6034322e93d00336f3
Raphael Silva Moreira <raphael.moreira@norus.com.br>
2023-09-25 12:00:29 -0300

    Abrindo escopo do `Resource.Shared`, de `ìnternal` para `public`

--- src/Lensophy.Domain/Resource/Shared.Designer.cs
+++ src/Lensophy.Domain/Resource/Shared.Designer.cs
@@ -21,7 +21,7 @@ namespace Lensophy.Domain.Resource {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Shared {
+    public class Shared {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -35,7 +35,7 @@ namespace Lensophy.Domain.Resource {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Lensophy.Domain.Resource.Shared", typeof(Shared).Assembly);
@@ -50,7 +50,7 @@ namespace Lensophy.Domain.Resource {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -62,7 +62,7 @@ namespace Lensophy.Domain.Resource {
         /// <summary>
         ///   Looks up a localized string similar to Analyze the message: `{0}`..
         /// </summary>
-        internal static string AnalyseTheMessage {
+        public static string AnalyseTheMessage {
             get {
                 return ResourceManager.GetString("AnalyseTheMessage", resourceCulture);
             }
@@ -71,7 +71,7 @@ namespace Lensophy.Domain.Resource {
         /// <summary>
         ///   Looks up a localized string similar to The straight word of Buddha: represents the importance of practicing honest, truthful, and benevolent communication that benefits both the speaker and the listener. Avoid speaking ill or harming someone&apos;s reputation. Do not use rude language, insults, or words that may hurt or inflict emotional suffering..
         /// </summary>
-        internal static string BuddhaVirtue {
+        public static string BuddhaVirtue {
             get {
                 return ResourceManager.GetString("BuddhaVirtue", resourceCulture);
             }
@@ -80,7 +80,7 @@ namespace Lensophy.Domain.Resource {
         /// <summary>
         ///   Looks up a localized string similar to The trustworthy disciple of Confucius: seeks to emphasize the importance of ethics in interpersonal relationships and the pursuit of moral virtue. Strives to do what is right in all situations and cultivates virtues such as compassion, honesty, and respect..
         /// </summary>
-        internal static string ConfuciusVirtue {
+        public static string ConfuciusVirtue {
             get {
                 return ResourceManager.GetString("ConfuciusVirtue", resourceCulture);
             }
@@ -89,7 +89,7 @@ namespace Lensophy.Domain.Resource {
         /// <summary>
         ///   Looks up a localized string similar to Considering the following context: &apos;{0}&apos;..
         /// </summary>
-        internal static string ConsideringFollowingContext {
+        public static string ConsideringFollowingContext {
             get {
                 return ResourceManager.GetString("ConsideringFollowingContext", resourceCulture);
             }
@@ -98,7 +98,7 @@ namespace Lensophy.Domain.Resource {
         /// <summary>
         ///   Looks up a localized string similar to Considering the following virtues: the triple filter of Socrates, the righteous word of Buddha and the trustworthy disciple of Confucius.
         /// </summary>
-        internal static string ConsideringFollowingVirtues {
+        public static string ConsideringFollowingVirtues {
             get {
                 return ResourceManager.GetString("ConsideringFollowingVirtues", resourceCulture);
             }
@@ -107,7 +107,7 @@ namespace Lensophy.Domain.Resource {
         /// <summary>
         ///   Looks up a localized string similar to Develop the answer without exceeding the limit of `{0}` tokens from the Api..
         /// </summary>
-        internal static string DoNotExceedMaxTokens {
+        public static string DoNotExceedMaxTokens {
             get {
                 return ResourceManager.GetString("DoNotExceedMaxTokens", resourceCulture);
             }
@@ -116,7 +116,7 @@ namespace Lensophy.Domain.Resource {
         /// <summary>
         ///   Looks up a localized string similar to Rewrite the message based on the mentioned virtues without changing or corrupting the original meaning of the message, without mentioning any of the virtues..
         /// </summary>
-        internal static string RewriteMessage {
+        public static string RewriteMessage {
             get {
                 return ResourceManager.GetString("RewriteMessage", resourceCulture);
             }
@@ -125,7 +125,7 @@ namespace Lensophy.Domain.Resource {
         /// <summary>
         ///   Looks up a localized string similar to Synthesize the answer in 20 words or less..
         /// </summary>
-        internal static string SaitamaVirtue {
+        public static string SaitamaVirtue {
             get {
                 return ResourceManager.GetString("SaitamaVirtue", resourceCulture);
             }
@@ -134,7 +134,7 @@ namespace Lensophy.Domain.Resource {
         /// <summary>
         ///   Looks up a localized string similar to Socrates&apos; Triple Filter: promotes kindness and considers the utility of our words. Words and actions should be beneficial to all involved, promoting well-being and harmony. Information should serve a constructive purpose and contribute to knowledge or progress..
         /// </summary>
-        internal static string SocratesVirtue {
+        public static string SocratesVirtue {
             get {
                 return ResourceManager.GetString("SocratesVirtue", resourceCulture);
             }
