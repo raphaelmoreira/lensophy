7c6a05e0230916b3224bb16894e44a0674aba92c
Raphael Silva Moreira <raphael.moreira@norus.com.br>
2023-09-27 12:22:07 -0300

    Traduzindo documentação do `Lensophy` para `en-US`

--- src/Lensophy/Lensophy.cs
+++ src/Lensophy/Lensophy.cs
@@ -1,24 +1,22 @@
-﻿using Lensophy.Domain.Dto.OpenAi;
-using Lensophy.Infrastructure.LargeLanguageModel;
-
-namespace Lensophy;
+﻿namespace Lensophy;
 
 /// <summary>
-/// Cada instância configura e inicia a biblioteca para avaliação e sugestão de comentários.
+/// Provides a class for message analysis, returning a gentle suggestion if the content is offensive in any way.
 /// </summary>
+/// <remarks>A service account is required to use it.</remarks>
 public class Lensophy
 {
     /// <summary>
-    /// Cria uma nova instância da biblioteca.
+    /// Initializes a new instance of the <see cref="Lensophy"/> class using a <see cref="OpenAiConfig"/>.
     /// </summary>
-    /// <param name="httpClient">O cliente http corrente.</param>
-    /// <param name="config">A configuração do OpenAi.</param>
-    /// <returns>Retorna a interface de uso.</returns>
-    /// <remarks><para>Se estiver numa aplicação web, evite criar <see cref="HttpClient"/> usando o keyword <b>new</b>.
-    /// Sempre repasse a instância corrente ou gerida através do <see cref="IHttpClientFactory"/>.</para>
-    /// <para>Mesmo que esteja envolto do escopo <b>using</b>, você não terá controle de quando o <see cref="HttpClient"/>
-    /// será descartado, podendo gerar mais instâncias do que o Garbage Collector é capaz de liberar,
-    /// ocasionando um <see cref="SocketsHttpHandler"/>.</para>
+    /// <param name="httpClient">The current HTTP instance (see remark).</param>
+    /// <param name="config">An OpenAI account <b>secret</b> is required to use it.</param>
+    /// <returns>Returns the <see cref="ILensophyLanguageModel"/> interface.</returns>
+    /// <remarks><para>Avoid creating a <b>new</b> instance of the <see cref="HttpClient"/> class. Instead, consider
+    /// passing the current request's instance or preferably injecting it through the <see cref="IHttpClientFactory"/>.</para>
+    /// <para>Even when wrapped in a <b>using</b> scope, you may not have control over when the <see cref="HttpClient"/> is
+    /// disposed, potentially leading to more instances than the Garbage Collector can release, resulting in a
+    /// <see cref="SocketException"/> issue.</para>
     /// </remarks>
     public static ILensophyLanguageModel CreateWithOpenAi(HttpClient httpClient, OpenAiConfig config)
     {
